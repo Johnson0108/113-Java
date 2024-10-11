42f6a495ad345daada99b7089dd926d19054b3a7
Johnson0108 <A111222020@mail.shu.edu.tw>
2024-10-11 11:56:01 +0800

    113-10-11

--- 113-10-11/Average.java
+++ 113-10-11/Average.java
@@ -2,10 +2,10 @@
 
 public class Average {
     public static void main(String[] args) {
-        var sum = 0;
-        for(var arg : args) {
+        int sum = 0;
+        for(String arg : args) {
             sum += Long.parseLong(arg);
         }
-        System.out.println("平均：" + (float) sum / args.length);
+        System.out.println("平均：" + (double) sum / args.length);
     }
 }
