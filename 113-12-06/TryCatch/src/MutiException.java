34c5a6d315d3103a382b573e7680ab8c649ea482
Johnson0108 <A111222020@mail.shu.edu.tw>
2024-12-06 12:02:26 +0800

    113-12-06

---
+++ 113-12-06/TryCatch/src/MutiException.java
@@ -0,0 +1,20 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
+
+public class MutiException {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3};
+        try {
+            File file = new File("nonexistentfile.txt");
+            FileReader reader = new FileReader(file);
+            System.out.println(numbers[5]);
+        } catch (FileNotFoundException e) {
+            System.out.println("檔案不存在" + e.getMessage());
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("陣列索引超出範圍" + e.getMessage());
+        }
+    }
+}
